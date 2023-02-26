d9fbf38bef0eea5b114ca0a47730c4961085ce2e
Analogium <lambertheo@gmail.com>
2023-02-26 18:31:26 +0100

    header encoche iphone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, NgModule } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { SafeArea, SafeAreaInsets } from 'capacitor-plugin-safe-area';
 
 
 @Component({
@@ -10,7 +12,17 @@ import { Component, NgModule } from '@angular/core';
 
 export class AppComponent {
 
-  constructor() { }
+  constructor() { 
+    if (Capacitor.getPlatform() === 'ios') {
+      SafeArea.getSafeAreaInsets().then((insets: SafeAreaInsets) => {
+        const header = document.getElementsByTagName('header')[0];
+        header.style.paddingTop = `${insets.insets.top}px`;
+        header.style.paddingBottom = `${insets.insets.bottom}px`;
+        header.style.paddingLeft = `${insets.insets.left}px`;
+        header.style.paddingRight = `${insets.insets.right}px`;
+      });
+    }
+  }
 
   ngOnInit(): void {
   }
