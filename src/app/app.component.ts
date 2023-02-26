39c94b453a829338b0e9812ece3a332fc00ee691
Analogium <lambertheo@gmail.com>
2023-02-26 18:49:41 +0100

    safe area exlude

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, NgModule } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
-import { SafeArea, SafeAreaInsets } from 'capacitor-plugin-safe-area';
 
 
 @Component({
@@ -13,15 +12,6 @@ import { SafeArea, SafeAreaInsets } from 'capacitor-plugin-safe-area';
 export class AppComponent {
 
   constructor() { 
-    if (Capacitor.getPlatform() === 'ios') {
-      SafeArea.getSafeAreaInsets().then((insets: SafeAreaInsets) => {
-        const header = document.getElementsByTagName('header')[0];
-        header.style.paddingTop = `${insets.insets.top}px`;
-        header.style.paddingBottom = `${insets.insets.bottom}px`;
-        header.style.paddingLeft = `${insets.insets.left}px`;
-        header.style.paddingRight = `${insets.insets.right}px`;
-      });
-    }
   }
 
   ngOnInit(): void {
